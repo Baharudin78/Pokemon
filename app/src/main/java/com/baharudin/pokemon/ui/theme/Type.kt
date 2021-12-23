54ee327956bd7fb8623ac95ea173367fd5d13604
Baharudin78 <bencoxgame78@gmail.com>
2021-12-23 22:30:22 +0700

    setup project

--- app/src/main/java/com/baharudin/pokemon/ui/theme/Type.kt
+++ app/src/main/java/com/baharudin/pokemon/ui/theme/Type.kt
@@ -2,14 +2,25 @@ package com.baharudin.pokemon.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.baharudin.pokemon.R
+
+val Nesa = FontFamily(
+    Font(R.font.nesa_light, FontWeight.Light),
+    Font(R.font.nesa_bold, FontWeight.Bold),
+)
+val NesaCondensered = FontFamily(
+    Font(R.font.nesa_light, FontWeight.Light),
+    Font(R.font.nesa_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Nesa ,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
